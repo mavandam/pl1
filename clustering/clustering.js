71dba97575e588158387b52954aa639003edbdd4
mavandam <91283287+mavandam@users.noreply.github.com>
2022-01-31 20:35:50 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -18,7 +18,8 @@ function initMap() {
   // Add a marker clusterer to manage the markers.
   new MarkerClusterer(map, markers, {
     imagePath:
-      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+      "https://www.ontarioparks.com/images/svg/op-logo-vertical-colour.svg",  
+    //"https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
   });
 }
 const locations = [
