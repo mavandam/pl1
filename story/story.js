d3467b41f3725e937e7a75c852ce9ac0d16a4e35
mavandam <91283287+mavandam@users.noreply.github.com>
2022-02-02 17:06:52 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -1,12 +1,13 @@
 function initMap() {
-  const mt = { lat: 43.633450414596815, lng: -79.4252901164058 };
+  //const mt = { lat: 43.633450414596815, lng: -79.4252901164058 };
+  const mt = { place_id: "ChIJPWn1Hgg1K4gRHRZxCH_uUCQ" };
   const cl = { lat: 43.6786945083944, lng: -79.40937514815542 };
   const osc = { lat: 43.71710641036828, lng: -79.33894362698436 };
   const cw = { lat: 43.84311266031023, lng: -79.53945611044928 };
   
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10,
-    center: mt,
+    center: cl
    });
   const m_mt = new google.maps.Marker({
     position: mt,
@@ -15,14 +16,17 @@ function initMap() {
   });
   const m_cl = new google.maps.Marker({
     position: cl,
+    label: "Casa Loma",
     map: map,
   });
   const m_osc = new google.maps.Marker({
     position: osc,
+    label: "Ontario Science Centre",
     map: map,
   });
   const m_cw = new google.maps.Marker({
     position: cw,
+    label: "Canada's Wonderland",
     map: map,
   });
 }
